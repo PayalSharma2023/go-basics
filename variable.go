575a3031c3cd48d24471c5e07685999a1c9242ea
Payal Sharma <payalsharmareal@gmail.com>
2024-02-06 21:40:36 +0530

    varaible declaration

---
+++ variable.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var fName string = "Payal"
+	var lName = "Sharma"
+
+	age := 19
+
+	fmt.Println("Hello ", fName, " ", lName, ". You are ", age, " years old")
+
+}
