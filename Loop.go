05ad5828153015f2f3c9de4f9bdc7650eeb6c2c5
PAYAL SHARMA <143309764+PayalSharma2023@users.noreply.github.com>
2024-02-13 19:36:29 +0530

    Create Loop.go

---
+++ Loop.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main () {
+  //for loop
+  for i = 0; i<5; i++ {
+    fmt.Println(i, " ")
+  }
+  fmt.Println()
+  // While Loop
+	j := 0
+
+	for j < 5 {
+		fmt.Print(j, " ")
+		j++
+  }
+}
