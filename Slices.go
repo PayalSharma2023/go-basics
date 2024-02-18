11ece77d37deb41aea253e955893ef2a475e045c
Payal Sharma <payalsharmareal@gmail.com>
2024-02-18 18:13:52 +0530

    changes

--- Slices.go
+++ Slices.go
@@ -1,24 +1,24 @@
-package main
+// package main
 
-import "fmt"
+// import "fmt"
 
-func printRoute(slice [] string) {
-  for i := 0; i < len(slice); i++ {
-    element := slice[i]
-    fmt.println(element)
-  }
-}
+// func printRoute(slice [] string) {
+//   for i := 0; i < len(slice); i++ {
+//     element := slice[i]
+//     fmt.println(element)
+//   }
+// }
 
-func main() {
-  route := [] string {"delhi", "Mumbai"}
-  printRoute(route)
+// func main() {
+//   route := [] string {"delhi", "Mumbai"}
+//   printRoute(route)
 
-  fmt.println()
-  route = append(route, "HP")
-  printRoute(route)
+//   fmt.println()
+//   route = append(route, "HP")
+//   printRoute(route)
 
-  fmt.println()
-  smallroute := route[0:2]
-  printRoute(smallroute)
-  
-}
+//   fmt.println()
+//   smallroute := route[0:2]
+//   printRoute(smallroute)
+
+// }
---
+++ basics/main.go
@@ -0,0 +1,91 @@
+// Non-Buffered Channel
+package main
+
+import "fmt"
+
+func main() {
+	myChan := make(chan string)
+
+	go func() {
+		for {
+			data := <-myChan
+			fmt.Println(data)
+		}
+	}()
+
+	myChan <- "hello world"
+	myChan <- "I'm learning channels"
+	myChan <- "This is extremely simple"
+}
+
+// Basic Channel
+package main
+
+import "fmt"
+
+func main() {
+	myChan := make(chan string, 2)
+
+	myChan <- "ABC"
+	myChan <- "XYZ"
+
+	opOne := <-myChan
+	opTwo := <-myChan
+
+	fmt.Println(opOne)
+	fmt.Println(opTwo)
+}
+
+// Mutex + WaitGroup
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+var mutex sync.Mutex
+var waitGroup sync.WaitGroup
+
+var result = []int{}
+
+func main() {
+	endpoints := []string{
+		"google.com", "twitter.com", "facebook.com",
+	}
+
+	for _, endpoint := range endpoints {
+		go getStatusCode(endpoint, &mutex, &waitGroup)
+		waitGroup.Add(1)
+	}
+
+	waitGroup.Wait()
+	fmt.Println(result)
+}
+
+func getStatusCode(website string, mt *sync.Mutex, wg *sync.WaitGroup) {
+	if website == "google.com" {
+		mt.Lock()
+		result = append(result, 200)
+		time.Sleep(time.Second * 1)
+		fmt.Println("Google Fetched")
+		mt.Unlock()
+	} else if website == "twitter.com" {
+		mt.Lock()
+		result = append(result, 200)
+		time.Sleep(time.Second * 2)
+		fmt.Println("Twitter Fetched")
+		mt.Unlock()
+	} else {
+		// For facebook -> 500 Internal Server Error
+		mt.Lock()
+		result = append(result, 500)
+		time.Sleep(time.Second * 3)
+		fmt.Println("Facebook Fetched")
+		mt.Unlock()
+	}
+
+	wg.Done()
+}
+
