f3ddb9aacbe23e69dde37319ece6a0388b0bcbcd
Payal Sharma <payalsharmareal@gmail.com>
2024-02-23 23:42:25 +0530

    WEB REQUEST AND URL

--- url/webrequest.go
+++ url/webrequest.go
@@ -2,25 +2,51 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"net/http"
+	"net/url"
 )
 
-const url string = "https://www.geeksforgeeks.org/golang/"
+const myurl string = "https://www.geeksforgeeks.org/golang/"
 
 func main() {
 	fmt.Println("Handling web requests with http")
-	response, err := http.Get(url)
+	response, err := http.Get(myurl)
 	if err != nil {
 		panic(err)
 	}
 	fmt.Printf("Response is of type : %T\n", response) //response has a header, body
 	defer response.Body.Close()
-	databyte, err := ioutil.ReadAll(response.Body)
+	//databyte, err := ioutil.ReadAll(response.Body)
 	if err != nil {
 		panic(err)
 	}
-	content := string(databyte)
-	fmt.Println(content)
+	//content := string(databyte)
+	//fmt.Println(content)
+
+	fmt.Println(myurl)
+	//Parsing
+	result, _ := url.Parse(myurl)
+	// fmt.Println(result.Scheme)
+	// fmt.Println(result.Host)
+	// fmt.Println(result.Path)
+	// fmt.Println(result.Port())
+	// fmt.Println(result.RawQuery)
+
+	qparams := result.Query()
+	fmt.Printf("the type of query params are : %T\n", qparams)
+
+	fmt.Println(qparams["pagename"])
+	for _, val := range qparams {
+		fmt.Println("Param is : ", val)
+	}
+
+	partsOfUrl := &url.URL{
+		Scheme:  "https",
+		Host:    "lco.dev",
+		Path:    "/tutcss",
+		RawPath: "user=Payal",
+	}
+	anotherurl := partsOfUrl.String()
+	fmt.Println(anotherurl)
 
 }
